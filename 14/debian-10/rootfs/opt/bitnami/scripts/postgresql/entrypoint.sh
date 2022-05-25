9319286b8b027460a1c61eebfe4d16ac547e22cf
gaultwe <gaultwe@l-gaultwe-1.statcan.ca>
2022-05-25 18:56:18 +0000

    wip: add hadoop

--- 14/debian-10/rootfs/opt/bitnami/scripts/postgresql/entrypoint.sh
+++ 14/debian-10/rootfs/opt/bitnami/scripts/postgresql/entrypoint.sh
@@ -14,6 +14,9 @@ set -o pipefail
 # Load PostgreSQL environment variables
 . /opt/bitnami/scripts/postgresql-env.sh
 
+export HADOOP_HOME=/opt/hadoop-3.2.0
+export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-3.2.0.jar
+
 print_welcome_page
 
 # Enable the nss_wrapper settings
@@ -26,7 +29,7 @@ if [[ "$*" = *"/opt/bitnami/scripts/postgresql/run.sh"* ]]; then
     info "** PostgreSQL setup finished! **"
 fi
 
-/opt/apache-hive-metastore-3.0.0-bin/bin/schematool -initSchema -dbType posgres
-/opt/apache-hive-metastore-3.0.0-bin/bin/start-metastore
+/apache-hive-metastore-3.0.0-bin/bin/schematool -initSchema -dbType posgres
+/apache-hive-metastore-3.0.0-bin/bin/start-metastore
 echo ""
 exec "$@"
